2665b9d758b84210cd791c1900cfe600da6065d2
jtx007 <jamesjacobthomas7@gmail.com>
2018-08-21 16:41:15 -0400

    added user creation in User controller

--- mod4backend/app/controllers/api/v1/users_controller.rb
+++ mod4backend/app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,26 @@ module Api
   module V1
     class UsersController < ApplicationController
 
+      def index
+
+        render json: User.all
+      end
+
+      def create
+         @user = User.new(user_params)
+
+        if @user.save
+          render json: @user, status: :created
+        else
+          render json: @user.errors, status: :unproccesable_entity
+        end
+      end
+      private
+
+      def user_params
+        params.require(:user).permit(:username)
+      end
+
     end
   end
 end
--- mod4backend/app/models/user.rb
+++ mod4backend/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :playlists
   has_many :songs, through: :playlists
+
+  
 end
